9cc2cc028d133a601a9b1b0fc3f59469a96c670c
Sarah Awik <sarah@sarahaweek.sarahawik>
2022-12-10 23:30:16 +0200

    Basic_Queries

---
+++ answers.sh
@@ -0,0 +1,7 @@
+. select \* from students
+SELECT * FROM students WHERE Age>30
+SELECT * FROM students WHERE Age=30 AND Gender="F"
+SELECT Points FROM students WHERE name='Alex'
+INSERT INTO students VALUES ('8','Sarahh', '26', 'F','500')
+UPDATE students SET Points=400 WHERE name='Basma'
+UPDATE students SET Points=190 WHERE name='Alex'
